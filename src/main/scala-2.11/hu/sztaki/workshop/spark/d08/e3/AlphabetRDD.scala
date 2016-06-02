e04740dc7c1435c9c9b1b5add920f330e9a778a6
zoltan.zvara <zoltan.zvara@gmail.com>
2016-06-02 11:27:53 -0500

    D9.

--- src/main/scala-2.11/hu/sztaki/workshop/spark/d08/e3/AlphabetRDD.scala
+++ src/main/scala-2.11/hu/sztaki/workshop/spark/d08/e3/AlphabetRDD.scala
@@ -5,15 +5,20 @@ import org.apache.spark.{Partition, SparkConf, SparkContext, TaskContext}
 
 class AlphabetRDD(prev:RDD[String]) extends RDD[String](prev){
   /**
-    * @todo[3] Decide if a string is an alphabet.
+    * @todo[3-HW] Decide if a string is an alphabet.
     */
-  def checkAlpha(str:String): Boolean = ???
+  def checkAlpha(str: String): Boolean = true
 
   /**
     * @todo[4] Override the compute method of the RDD.
-    *           Split the string and filter out the non-alphabets.
+    *          Split the string and filter out the non-alphabets.
     */
-  override def compute(split: Partition, context: TaskContext): Iterator[String] = ???
+  override def compute(split: Partition, context: TaskContext): Iterator[String] = {
+    firstParent[String]
+      .iterator(split, context)
+      .flatMap(_.split(" "))
+      .filter(checkAlpha)
+  }
 
   override protected def getPartitions: Array[Partition] = firstParent[String].partitions
   override val partitioner = firstParent[String].partitioner
@@ -24,15 +29,15 @@ class AlphabetRDD(prev:RDD[String]) extends RDD[String](prev){
 object AlphabetRDD {
   def main(args: Array[String]): Unit = {
     /**
-      * @todo[1] Setup again! Also, read from text file.
+      * @todo[1-HW] Setup again! Also, read from text file.
       */
 
     /**
-      * @todo[2] Count the records (line of the text file).
+      * @todo[2-HW] Count the records (line of the text file).
       */
 
     /**
-      * @todo[5] Create the AlphabetRDD from the general RDD
+      * @todo[5-HW] Create the AlphabetRDD from the general RDD
       *           and count the elements again.
       */
   }
